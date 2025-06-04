73e43484ade24c23602a56f3b1a65bc1a2c29d16
Bekhzod Allaev <icebreaker0210mail.ru>
2025-06-05 00:17:54 +0200

    add minimal projects page

---
+++ client/src/global.d.ts
@@ -0,0 +1,23 @@
+// For CSS Modules
+declare module '*.module.css' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+// For images
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.svg';
+
+// For fonts
+declare module '*.woff';
+declare module '*.woff2';
+declare module '*.ttf';
+declare module '*.eot';
+
+// For other files
+declare module '*.mp4';
+declare module '*.webm';
+declare module '*.json';
--- client/src/pages/Projects/Projects.tsx
+++ client/src/pages/Projects/Projects.tsx
@@ -1,8 +1,57 @@
 import React from 'react'
+import styles from './Projects.module.css'
+import gettyImages from '../../assets/projects/gettyimages.jpg'
+import music from '../../assets/projects/music.jpg'
+import taskManager from '../../assets/projects/taskmanager.jpg'
+import tenzie from '../../assets/projects/tenzie.jpg'
 
 function Projects() {
   return (
-    <div>Projects</div>
+    <div className={styles.projects_wrapper}>
+      <h1>Projects</h1>
+      <p>A curated selection of my recent work, highlighting my expertise in software development</p>
+      <div className={styles.projects_container}>
+        <div className={styles.project}>
+          <div className={styles.image_container}>
+            <a href="https://bekhzodallaev.github.io/getty-images/" target='_blank'>
+              <img src={gettyImages} alt="" />
+            </a>
+          </div>
+          <h3>Getty Images</h3>
+          <p>Getty Images helps find any images globally.</p>
+        </div>
+        
+        <div className={styles.project}>
+          <div className={styles.image_container}>
+            <a href="https://github.com/bekhzodallaev/Music-App" target='_blank'>
+            <img src={music} alt="" />
+            </a>
+          </div>
+          <h3>Music App</h3>
+          <p>Music app allows you upload music and listen.Lets make your music listening more enjoyable</p>
+        </div>
+        
+        <div className={styles.project}>
+          <div className={styles.image_container}>
+            <a href="https://github.com/bekhzodallaev/Task-Manager" target='_blank'>
+            <img src={taskManager} alt="" />
+            </a>
+          </div>
+          <h3>Task Manager</h3>
+          <p>Task Manager is a good choice to manage your tasks wisely.</p>
+        </div>
+        
+        <div className={styles.project}>
+          <div className={styles.image_container}>
+            <a href="https://github.com/bekhzodallaev/Task-Manager" target='_blank'>
+            <img src={tenzie} alt="" />
+            </a>
+          </div>
+          <h3>Tenzie Game</h3>
+          <p>Tenzie Game creates an exciting atmosphere to spend your quality time.Lets try and see how it works</p>
+           </div>
+      </div>
+    </div>
   )
 }
 
