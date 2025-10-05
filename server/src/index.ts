8146ce5153e31143c7e29192f21f68f8370d901e
Bekhzod Allaev <icebreaker0210mail.ru>
2025-10-05 14:04:28 +0200

    add latest changes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,15 @@
 import express from "express";
 import cors from "cors";
-import contactRoutes from "./routes/contact-message";
 import postsRoute from "./routes/posts";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/contact", contactRoutes); 
 app.use("/api/posts", postsRoute);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+g
